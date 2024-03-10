ec28d63cc6c03f2fc437ac4d2aca4f96697dcf41
IDB LAB .01 <IDB LAB .01@DESKTOP-43BVRGO>
2024-03-10 14:25:43 +0600

    Add project files.

---
+++ EvidenceExam9/Migrations/Configuration.cs
@@ -0,0 +1,41 @@
+﻿namespace EvidenceExam9.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EvidenceExam9.Models.AppDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+			AutomaticMigrationDataLossAllowed = true;
+		}
+
+        protected override void Seed(EvidenceExam9.Models.AppDb context)
+        {
+			//  This method will be called after migrating to the latest version.
+
+			//  You can use the DbSet<T>.AddOrUpdate() helper extension method
+			//  to avoid creating duplicate seed data.
+
+
+			context.Users.AddOrUpdate(new Models.UserInfo() { UserName = "admin", Password = "abc123", Role = "Admin" });
+
+
+			context.Users.AddOrUpdate(new Models.UserInfo() { UserName = "ditc", Password = "123456", Role = "User" });
+
+			
+
+
+
+
+
+
+
+
+
+		}
+	}
+}
---
+++ EvidenceExam9/Models/UserInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EvidenceExam9.Models
+{
+	public class UserInfo
+	{
+		//[Key]
+		//public Guid UserId { get; set; } = Guid.NewGuid();
+		[Key]
+		[Required]
+		[StringLength(50, MinimumLength = 4)]
+		public string UserName { get; set; }
+		[Required]
+		[StringLength(50, MinimumLength = 6)]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		//[EnumDataType(typeof(UserRole))]
+		public string Role { get; set; }
+
+	}
+
+	//public enum UserRole
+	//{
+	//    Admin, User, Student, Teacher
+      // }
+
+
+}
